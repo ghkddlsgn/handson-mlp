0beeba3c135f517f96a58db5241f4f40f57499b6
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2026-03-15 22:12:43 +0100

    study end to end machine learning exmaple

---
+++ study/StandardScalerClone.py
@@ -0,0 +1,22 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_array, check_is_fitted
+
+class StandardScalerClone(BaseEstimator, TransformerMixin):
+    def __init__(self, with_mean = True) -> None:
+        super().__init__()
+        self.with_mean = with_mean
+    
+    def fit(self, X, y=None):
+        X = check_array(X)
+        self.mean_ = X.mean(axis=0)
+        self.scale_ = X.std(axis=0)
+        self.n_features_in_ = X.shape[1]
+
+        return self
+    
+    def transform(self, X):
+        check_is_fitted(self)
+        X = check_array(X)
+        assert self.n_features_in_ == X.shape[1]
+        if self.with_mean:
+            pass
